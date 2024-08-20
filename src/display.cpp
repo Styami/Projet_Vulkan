a8d4ddd1ed4c198ae073b15130ae88ab408be65a
Styami <b.sachastep@gmail.com>
2024-08-20 02:21:46 +0200

    Ajout de fonction intermédiaire La fonction permet de simplément récupérer les extensions de paramétrage de vulkan.

--- src/display.cpp
+++ src/display.cpp
@@ -1,5 +1,6 @@
 #include "display.hpp"
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_vulkan.h>
 
 Display::Display() {
   is_running = true;
@@ -7,8 +8,13 @@ Display::Display() {
 
 void Display::initDisplay() {
   SDL_Init(SDL_INIT_VIDEO);
-  window = SDL_CreateWindow("vulkan", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 450, SDL_WINDOW_VULKAN);
+  window = SDL_CreateWindow("vulkan", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 450, SDL_WINDOW_SHOWN);
   SDL_SetWindowResizable(window, SDL_FALSE);
+}
+
+
+void Display::getExtensions(unsigned int& number, const char** names) {
+  SDL_Vulkan_GetInstanceExtensions(window, &number, names);
 }
 
 void Display::loop() {
--- src/display.hpp
+++ src/display.hpp
@@ -6,6 +6,12 @@ class Display {
   public:
     Display();
     void initDisplay();
+    
+    /*
+    simple utilisation en getteur de la fonction "getInstance" de SDL2.
+    Voir la doc pour plus d'info.
+    */
+    void getExtensions(unsigned int& number, const char** names);
     void loop();
     void clean();
 
@@ -13,5 +19,6 @@ class Display {
     SDL_Window *window;
     SDL_Event events;
     SDL_Renderer *renderer;
+    
     bool is_running;
 };
